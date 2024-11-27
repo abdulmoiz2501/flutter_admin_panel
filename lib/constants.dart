8045572a57245151e0af54a4565217c967a1eb23
Abdul Moiz <82509886+abdulmoiz2501@users.noreply.github.com>
2024-11-27 20:26:57 +0500

    final

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+/*const primaryColor = Color(0xFF2697FF);
+const secondaryColor = Color(0xFF2A2D3E);
+const bgColor = Color(0xFF212332);*/
+const primaryColor = Color(0xFF4CAF50); // Calm Green
+const secondaryColor = Color(0xFF1E1E2F); // Dark Gray
+const bgColor = Color(0xFF121212);
+
+const defaultPadding = 16.0;
